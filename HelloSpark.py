6468b7ee43338b4bbbd5d2e767b2415e88b2c0c5
MeghaAgarwal06 <meg.agrwal@gmail.com>
2021-08-16 12:22:13 +1000

    modularised

--- HelloSpark.py
+++ HelloSpark.py
@@ -3,7 +3,7 @@ import sys
 from pyspark import SparkConf
 from pyspark.sql import *
 from lib.logger import Log4J
-from lib.utils import get_spark_config, load_survey_df, count_by_country
+from lib.utils import get_spark_config, load_survey_df, clean_data
 from pyspark.sql import functions
 
 if __name__ == "__main__":
@@ -20,18 +20,14 @@ if __name__ == "__main__":
         logger.error("usage:HelloSpark data_raw_data000000000000")
         sys.exit(-1)
 
-    schemaDDL = """domain_userid STRING, network_userid STRING, domain_sessionid STRING, useragent STRING, os_name STRING,os_family STRING,br_name STRING,br_family STRING,br_version STRING,
-        br_type STRING, derived_tstamp STRING,collector_tstamp STRING,page_urlscheme STRING, page_urlhost STRING,page_urlport INT, page_urlpath STRING, refr_urlscheme STRING, refr_urlhost STRING, refr_urlport INT, refr_urlpath STRING,
-        app_id STRING, user_ipaddress STRING,page_view_id STRING, member_id STRING, member_type STRING, article_id STRING, article_type STRING,article_primary_category STRING"""
 
-    complete_df = spark.read. \
-        option("header", "true"). \
-        format("csv").schema(schemaDDL). \
-        load(sys.argv)
+    #Loading the data into a data frame
 
-    complete_df.createOrReplaceTempView("compsurvey_tbl")
-    complete_df.show()
-    logger.info("Finished HelloSpark")
+    complete_df=load_survey_df(spark,sys.argv[1])
+
+
+    #complete_df.show()
+    logger.info("Finished Loading the data to a dataframe")
 
     # Data cleansing
     # 1.Remove Unwanted fields
@@ -39,34 +35,16 @@ if __name__ == "__main__":
     # 3.Identify junk records and delete
     # 4.Converting string to datetime
 
-    complete_df.drop(complete_df.app_id)
-
-    # removing duplicates from data
-    distinctDF = complete_df.distinct()
-    print("Distinct count: ", distinctDF.count())
-
-    # converting string to datetime formats
-    df = spark.sql(
-        " select  substr(derived_tstamp,1,22) as derived_tstp ,substr(collector_tstamp,1,22) as collector_tstp,* FROM compsurvey_tbl where page_view_id is not null")
-
-    dfNew = df.select(("domain_userid"), col("network_userid"), col("domain_sessionid"), col("useragent"),
-                      col("os_name"), col("os_family"), col("br_name"), col("br_family"), col("br_version"),
-                      col("br_type"), col("page_urlscheme"), col("page_urlhost"), col("page_urlport"),
-                      col("page_urlpath"), col("refr_urlscheme"), col("refr_urlhost"), col("refr_urlport"),
-                      col("refr_urlpath"), col("user_ipaddress"), col("page_view_id"), col("member_id"),
-                      col("member_type"), col("article_id"), col("article_type"), col("article_primary_category"),
-                      to_timestamp(col("derived_tstp"), "yyyy-MM-dd HH:mm:ss.SS").alias("derived_tstamp"),
-                      to_timestamp(col("collector_tstp"), "yyyy-MM-dd HH:mm:ss.SS").alias("collector_tstamp"))
-    # dfNew.show(1,False)
+    dfNew=clean_data(spark,complete_df)
     dfNew.createOrReplaceTempView("final_tbl")
 
     # Event  modelling for workflows
-
+    # Picking historical year as 2020
     df2020 = spark.sql(
         "select *f rom final_tbl where (derived_tstamp>'2019-12-31 23:59:59' and derived_tstamp<='2020-12-31 23:59:59')")
     df2020.createOrReplaceTempView("year2020tbl")
 
-    # Gather Basic information for 2020 year
+    # Gather Basic information for 2020 year like number of users, sessions, page views
     basicInfoDf = spark.sql(
         "SELECT  COUNT(DISTINCT domain_userid) AS user_count,COUNT(DISTINCT domain_sessionid) AS session_count,count(page_view_id),MIN(derived_tstamp) AS first_event,MAX(derived_tstamp) AS last_event from year2020tbl ")
 
--- lib/utils.py
+++ lib/utils.py
@@ -1,5 +1,6 @@
 import configparser
 from pyspark import SparkConf
+from pyspark.sql import *
 
 
 def get_spark_config():
@@ -14,15 +15,36 @@ def get_spark_config():
 
 
 def load_survey_df(spark, data_file):
-    return spark.read \
-        .option("header", "true") \
-        .option("inferSchema", "true") \
-        .csv(data_file)
+    schemaDDL = """domain_userid STRING, network_userid STRING, domain_sessionid STRING, useragent STRING, os_name STRING,os_family STRING,br_name STRING,br_family STRING,br_version STRING,
+            br_type STRING, derived_tstamp STRING,collector_tstamp STRING,page_urlscheme STRING, page_urlhost STRING,page_urlport INT, page_urlpath STRING, refr_urlscheme STRING, refr_urlhost STRING, refr_urlport INT, refr_urlpath STRING,
+            app_id STRING, user_ipaddress STRING,page_view_id STRING, member_id STRING, member_type STRING, article_id STRING, article_type STRING,article_primary_category STRING"""
+
+    return spark.read. \
+        option("header", "true"). \
+        format("csv").schema(schemaDDL). \
+        load(data_file)
 
 
-def count_by_country(survey_df):
-    return survey_df \
-        .where("Age<40") \
-        .select("Age", "Gender", "Country", "state") \
-        .groupBy("Country") \
-        .count()
+
+def clean_data(spark,complete_df):
+    complete_df.createOrReplaceTempView("compsurvey_tbl")
+    complete_df.drop(complete_df.app_id)
+
+    # removing duplicates from data
+    distinctDF = complete_df.distinct()
+    # print("Distinct count: ", distinctDF.count())
+
+    # converting string to datetime formats
+    df = spark.sql(
+        " select  substr(derived_tstamp,1,22) as derived_tstp ,substr(collector_tstamp,1,22) as collector_tstp,* FROM compsurvey_tbl where page_view_id is not null")
+
+    dfNew = df.select(("domain_userid"), col("network_userid"), col("domain_sessionid"), col("useragent"),
+                      col("os_name"), col("os_family"), col("br_name"), col("br_family"), col("br_version"),
+                      col("br_type"), col("page_urlscheme"), col("page_urlhost"), col("page_urlport"),
+                      col("page_urlpath"), col("refr_urlscheme"), col("refr_urlhost"), col("refr_urlport"),
+                      col("refr_urlpath"), col("user_ipaddress"), col("page_view_id"), col("member_id"),
+                      col("member_type"), col("article_id"), col("article_type"), col("article_primary_category"),
+                      to_timestamp(col("derived_tstp"), "yyyy-MM-dd HH:mm:ss.SS").alias("derived_tstamp"),
+                      to_timestamp(col("collector_tstp"), "yyyy-MM-dd HH:mm:ss.SS").alias("collector_tstamp"))
+    # dfNew.show(1,False)
+    return dfNew
